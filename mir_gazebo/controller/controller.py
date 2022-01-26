2d89cdc63d14d2e7865ffbdbba26fd9e57efd7f5
Your Name <you@example.com>
2022-01-26 09:44:10 +0800

    second

--- mir_gazebo/controller/controller.py
+++ mir_gazebo/controller/controller.py
@@ -4,10 +4,12 @@ from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Point, Twist
 from math import atan2
+import math
 
 x = 0
 y = 0
 theta = 0
+rotate = True
 
 def newOdom(msg):
     global x
@@ -43,25 +45,44 @@ while not rospy.is_shutdown():
 
     inc_x = goal.x -x
     inc_y = goal.y -y
-
+ 
     angle_to_goal = atan2(inc_y, inc_x)
-    print(x, y)
+    
 
-    if abs(abs(angle_to_goal) - abs(theta)) > 0.1:
-	
-        speed.linear.x = 0.0
-        speed.angular.z = 0.3
-    else:
-	if position(goal.x, goal.y, x, y) <0.5:
-		print("i am here")
+    if theta <0:
+	theta += 3.14*2
+
+    if angle_to_goal <0:
+	angle_to_goal += 3.14*2
+
+    if theta>6.18:
+	theta -=6.28
+    if angle_to_goal>6.18:
+	angle_to_goal -=6.28
+
+
+
+    if position(goal.x, goal.y, x, y) <0.1:
+		print("arrived")
 		if goal_list:
 			curr_point = goal_list.pop(0)
 			goal.x = x + curr_point[0]
 			goal.y = y + curr_point[1]
+			
 		else:
 			break
-        speed.linear.x = 0.5
-        speed.angular.z = 0.0
+    elif abs(angle_to_goal-theta) > 0.1 :
+
+        	speed.linear.x = 0.0
+        	speed.angular.z = 0.3
+	
+	
+    else:
+	
+	
+	        speed.linear.x = 0.5
+       		speed.angular.z = 0.0
+		
 
     pub.publish(speed)
 
